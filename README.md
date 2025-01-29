1709dc9f57a4b6a6a911ddb13e5fd9c4db795f51
msadath <msadath@vt.edu>
2025-01-29 16:09:23 -0500

    Portfolio Base and structure

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# portfolio
+# Personal-Portfolio
+Hey! This is my portfolio.
+<br>
+  <hr>
+
+***Cloning repo in local machine***
+- click on the code button (green colour button on right side)
+- click on the download zip (the last one in the drop-down)
+- extract the zip file and open the folder in vs-code
+- then run below commands in the terminal 
+  
+**To run on local machine**
+```js
+npm i
+npm run dev
+```
+
+**PS: Don't forget to star the repo (top right most button) it motivates me to make more such websites.**
+
+
+
+
---
+++ src/constants/Constant.jsx
@@ -0,0 +1,55 @@
+import C from "/C.png";
+import CPP from "/CPP.png";
+import HTML from "/HTML.png";
+import Github from "/Github.png";
+import CSS from "/CSS.png";
+import Python from "/Python.png";
+import Javascript from "/Javascript.png";
+import Git from "/Git.png";
+import Vite from "/Vite.png";
+import TailwindCSS from "/TailwindCSS.png";
+import react from "/react.png";
+import Vue from "/vue.png";
+
+import Hackthechain from "/HackTheChainLogo.png";
+import GSSOC from "/GSSoc.jpg";
+import IITB from "/IITB.png";
+import Sports from "/Sports_Club.png";
+import IIITians from "/IIITians_Network.png";
+import GDSC from "/GDSC.png";
+import KDE from "/kde.png";
+import Oppia from "/oppia.png";
+
+import GPT4 from "/gpt4.jpg";
+import Portfolio from "/portfolio.jpg";
+import Rocket from "/Rocket.jpg";
+import Game from "/Stone.jpg";
+import WellBeing from "/well-being.jpg";
+
+export {
+  C,
+  CPP,
+  HTML,
+  CSS,
+  Github,
+  Git,
+  Vite,
+  TailwindCSS,
+  react,
+  Python,
+  Javascript,
+  Vue,
+  Hackthechain,
+  GSSOC,
+  IITB,
+  Sports,
+  IIITians,
+  GPT4,
+  Portfolio,
+  Rocket,
+  WellBeing,
+  Game,
+  GDSC,
+  KDE,
+  Oppia,
+};
